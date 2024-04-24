d9a15330fd875da8a8e7f06acd17d0b45643640d
Andrea Milošević <135378482+Andrea409@users.noreply.github.com>
2024-04-24 11:52:11 +0200

    Completed the test.

--- page-objects/myProfilePage.ts
+++ page-objects/myProfilePage.ts
@@ -1,4 +1,4 @@
-import { Page, expect } from '@playwright/test';
+import { Page } from '@playwright/test';
 import { HelperBase } from './helperBase';
 
 
@@ -7,4 +7,49 @@ export class MyProfilePage extends HelperBase {
     constructor(page: Page){
         super(page)
     }
+    async editProfile(){
+        await this.page.getByText("Edit Profile").click()
+    }
+
+    async firstName(){
+        await this.page.locator('#firstname').fill('Andrea')
+    }
+    async lastName(){
+        await this.page.locator('#lastname').fill('Milsevic')
+    }
+    async adress1(){
+        await this.page.locator('#address1').fill('Ljubovija')
+    }
+    async adress2(){
+        await this.page.locator('#address2').fill('Beograd')
+    }
+    async City(){
+        await this.page.locator('#addressCity').fill('NovI Sad')
+
+    }
+    async zipCode(){
+        await this.page.locator('#addressZipcode').fill('15320')
+
+    }
+    async phoneNumberField(){
+        await this.page.locator('#phone').fill('+38164333961')
+
+    }
+    async state(stateTitle:string){
+       const inputElement = await this.page.waitForSelector('[aria-labelledby="vs1__combobox"]');
+       await inputElement.click()
+       await this.page.locator('li[role="option"]', {hasText: stateTitle}).click()
+    }
+    async SaveProfileButton(){
+      await this.page.getByText("Save profile").click()
+
+    }
+    async BannerText() {
+         const element = this.page.locator('.inline-link').getByText("View updated profile");
+         const text = await element.innerText();
+         return text;
+
+    }
+    
+
 }
--- tests/myProfile.spec.ts
+++ tests/myProfile.spec.ts
@@ -8,27 +8,46 @@ test.beforeEach(async ({page})=>{
     
     await page.goto('https://www.shopist.io/')
     })
-
-
     
     test('Check sold out product', async ({ page }) => {
         
         const navigateTo = new NavigationPage(page);
         const onMyProfilePage = new MyProfilePage(page);
 
+        //navigate to MyProfilePage
         await navigateTo.myProfilePage()
-        await onMyProfilePage.editProfile() 
-        await onMyProfilePage.firstName()
-        await onMyProfilePage.lastName()
-        await onMyProfilePage.adress1()
-        await onMyProfilePage.adress2()
-        await onMyProfilePage.City()
-        await onMyProfilePage.zipCode()
-        await onMyProfilePage.phoneNumberField()
-        await onMyProfilePage.state("DE")
-        await onMyProfilePage.SaveProfile()
 
-        const bannerText = await onMyProfilePage.BannerText()
-        expect(bannerText).toContain('Profile successfully saved. View updated profile')
+        //Click on 'EDIT PROFILE' button
+        await onMyProfilePage.editProfile() 
+
+        //Data entry for the first name field
+        await onMyProfilePage.firstName()
+
+        //Data entry for the last name field
+        await onMyProfilePage.lastName()
+
+        //Data entry for the adress field
+        await onMyProfilePage.adress1()
+
+        //Data entry for the adress2 field.
+        await onMyProfilePage.adress2()
+
+        //Data entry for the City field.
+        await onMyProfilePage.City()
+
+        //Data entry for the Zipcode field.
+        await onMyProfilePage.zipCode()
+
+        //Data entry for the Mobile phone number field.
+        await onMyProfilePage.phoneNumberField()
+
+        //Selecting a value from a list.
+        await onMyProfilePage.state("DE")
+
+        //Click on the 'SAVE PROFILE' button
+        await onMyProfilePage.SaveProfileButton()
+
+        //assertation
+        expect(await onMyProfilePage.BannerText()).toContain('View updated profile');
        
     })
