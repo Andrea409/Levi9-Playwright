3276eeac41ae49b36aefd3e949c352baafef3461
Andrea Milošević <135378482+Andrea409@users.noreply.github.com>
2024-04-25 09:08:33 +0200

    Until assertation

--- page-objects/beddingPage.ts
+++ page-objects/beddingPage.ts
@@ -6,4 +6,16 @@ export class BeddingPage extends HelperBase {
     constructor(page: Page){
         super(page)
     }
+    async clickOnWhiteLinenDuvetCover(){
+        await this.page.locator('.product-card-container > div > a').first().click()
+    }
+    async clickOnAddToCartButton(){
+        await this.page.getByText("Add to cart").click()
+    }
+    async navigateToCartButton(){
+        await this.page.getByText("Cart (1)").first().click()
+    }
+    async clickOnRemoveButton(){
+        await this.page.getByText("Remove").click()
+    }
 }
---
+++ tests/RemoveItemFromBag.spec.ts
@@ -0,0 +1,27 @@
+import {test} from '@playwright/test'
+import {NavigationPage } from '../page-objects/navigationPage'
+import {BeddingPage} from '../page-objects/beddingPage'
+
+
+
+test.beforeEach(async ({page})=>{
+    
+    await page.goto('https://www.shopist.io/')
+    })
+    
+    test('Remove item from the shopping bag', async ({ page }) => {
+        
+        const navigateTo = new NavigationPage(page);
+        const onBeddingPage = new BeddingPage(page);
+
+        //navigate to MyProfilePage
+        await navigateTo.beddingPage()
+
+        await onBeddingPage.clickOnWhiteLinenDuvetCover()
+        await onBeddingPage.clickOnAddToCartButton()
+        await onBeddingPage.navigateToCartButton()
+        await onBeddingPage.clickOnRemoveButton()
+
+    }
+   
+)
