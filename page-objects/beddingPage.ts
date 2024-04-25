8fa737a9c5353c277d69bcaa53ff4f7fbd07c214
Andrea Milošević <135378482+Andrea409@users.noreply.github.com>
2024-04-25 16:52:19 +0200

    completed test

--- page-objects/beddingPage.ts
+++ page-objects/beddingPage.ts
@@ -34,3 +34,5 @@ export class BeddingPage extends HelperBase {
 
 
 
+
+
--- tests/CounterInShopingBag.spec.ts
+++ tests/CounterInShopingBag.spec.ts
@@ -2,13 +2,11 @@ import {test, expect} from '@playwright/test'
 import {NavigationPage } from '../page-objects/navigationPage'
 import {BeddingPage} from '../page-objects/beddingPage'
 
-
-
 test.beforeEach(async ({page})=>{
     
     await page.goto('https://www.shopist.io/')
-    
-    })
+
+})
     
     test('Counter in shopping bag', async ({ page }) => {
         
@@ -17,23 +15,68 @@ test.beforeEach(async ({page})=>{
 
         //navigate to Shop Now page 
         await navigateTo.shopNowPage()
+
         //navigate to Bedding page
         await navigateTo.beddingPage()
+
         //Click on the White Linen Duvet Cover 
         await onBeddingPage.clickOnWhiteLinenDuvetCover()
+
+        //price before cart page 
+        const initialPrice = await page.getByText('$').innerText()
+
+        // Removing the $ sign
+        const priceWithout$sign = initialPrice.replace(/[^\d.]/g, '');
+
+        // Parsing the string into a number
+        const parseValue = parseFloat(priceWithout$sign);
+
+        // The value of priceElementValue multiplied by 2
+        const initialValueMultiple2 = parseValue * 2;
+
+        //Displaying the value
+        console.log(initialValueMultiple2)
+
         //Click on the Add to Cart button
         await onBeddingPage.clickOnAddToCartButton()
+
         //Navigate ti Cart page
         await onBeddingPage.navigateToCartButton()
+
         //Click on the Quantity increase button 
         await onBeddingPage.quantityIncreaseButton()
 
-        await page.getByText('$').nth(1).click()
+         //price In the cart page 
+        const priceInTheCart = await page.getByText('$').nth(1).innerText()
+        
+        // Removing the $ sign
+        const deleted$Value = priceInTheCart.replace(/[^\d.]/g, '');
+
+        // Parsing the string into a number
+        const parsePricePriceFromCart = parseFloat(deleted$Value)
+        
+        //Displaying the value
+        console.log(parsePricePriceFromCart)
+       
+        //assertation-Validation of price correctness after adding a product
+        expect(initialValueMultiple2).toEqual(parsePricePriceFromCart)
+
         //Click on Reduce Poroduct Count button 
         await onBeddingPage.reduceProductCountButton()
+
+        //Value of element after click on reduce button
+        const reducePrice = await page.getByText('$').nth(1).innerText()
         
-       //assertation- 
-       // expect(await onBeddingPage.appearsTextAfterRemoveAllProducts()).toContain('Your cart is currently empty.')
-    }  
+        //assertation-Verification of equality between initial and reduced prices.
+        expect(initialPrice).toEqual(reducePrice)
+
+        //Displaying the value
+        console.log(reducePrice)
+
+        //Displaying the value
+        console.log(initialPrice)
+       
+    }
    
 )
+
